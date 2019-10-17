cd39ff85d9b58bac4333468f50e1d4e5adcbb708
flrasmith <50145365+flrasmith@users.noreply.github.com>
2019-10-17 09:59:55 -0600

    Update site.pp
    test with pipe for adding content with new line.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,10 @@
 node default {
   file {'/root/README':
     ensure  => file,
-    content => 'This is a rob readme',
+    content => | This is a rob readme
+                 and second line
+                 last line making it third line
+                 ,
     owner   => 'root',
   }
 }
